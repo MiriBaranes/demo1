617899020c6eb98791577e97f42e9b0c575cbf92
MiriBaranes <miralbrn@edu.aac.ac.il>
2024-02-14 16:43:42 +0200

    Utils

--- src/main/java/com/example/demo/util/Utils.java
+++ src/main/java/com/example/demo/util/Utils.java
@@ -1,5 +1,24 @@
 package com.example.demo.util;
 
+import jakarta.xml.bind.DatatypeConverter;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
 public class Utils {
+    public static String createHash (String username, String password) {
+        String myHash = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update((username + password).getBytes());
+            byte[] digest = md.digest();
+            myHash = DatatypeConverter
+                    .printHexBinary(digest).toUpperCase();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return myHash;
+    }
 
 }
